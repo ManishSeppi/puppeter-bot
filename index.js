73b201f0af13a9c65b1b096bd28e87190693f2a2
ManishSeppi <113444386+ManishSeppi@users.noreply.github.com>
2023-02-17 12:59:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ import { executablePath } from 'puppeteer';
 import fs from 'fs';
 import AudioRecorder from 'node-audiorecorder';
 import * as PuppeteerScreenRecorder from 'puppeteer-screen-recorder'
+import RecordRTC from 'recordrtc';
 
+import { getStream } from "puppeteer-stream";
+
+const file = fs.createWriteStream("./test.webm");
 
 puppeteer.use(StealthPlugin());
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: [],
-    ignoreDefaultArgs: false,
+    defaultViewport: null,
+    devtools: false,
+    args: [
+      "--window-size=1920,1080",
+      "--window-position=1921,0",
+      "--autoplay-policy=no-user-gesture-required",
+    ],
+    ignoreDefaultArgs: ["--mute-audio"],
     executablePath: executablePath(),
   });
 
@@ -25,7 +35,7 @@ puppeteer.use(StealthPlugin());
 
   // going to Meet after signing in
   await page.waitForTimeout(2500);
-  await page.goto('https://meet.google.com/mdy-ipnd-urm' + '?hl=en', {
+  await page.goto('https://meet.google.com/dnt-yhic-eyk' + '?hl=en', {
     waitUntil: 'networkidle0',
     timeout: 10000,
   });
@@ -63,12 +73,54 @@ puppeteer.use(StealthPlugin());
     `button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 jEvJdc QJgqC"]`
   );
 
+  const stream = await getStream(page, { audio: true, video: true});
+  console.log("recording");
+
+  stream.pipe(file);
+  // setTimeout(async () => {
+  //   await stream.destroy();
+  //   file.close();
+  //   console.log("finished");
+  // }, 1000 * 30);
+
+
   const recorder = new PuppeteerScreenRecorder.PuppeteerScreenRecorder(page);
   await recorder.start('./report/video/simple.webm'); // supports extension - mp4, avi, webm and mov
 
-  setTimeout(async() => {
+  // const devices = await page.evaluate(() =>
+  //   navigator.mediaDevices.getUserMedia(
+  //     { audio: true }
+  //   )
+  // )
+
+  // let x = await navigator.mediaDevices.getUserMedia({audio: true});
+
+  // console.log(x, "Available devices");
+  // navigator.mediaDevices.getUserMedia({
+  //   video: false,
+  //   audio: true
+  // }).then(async function (stream) {
+  //   let recorder = RecordRTC(stream, {
+  //     type: 'audio'
+  //   });
+  //   recorder.startRecording();
+
+  //   const sleep = m => new Promise(r => setTimeout(r, m));
+  //   await sleep(3000);
+
+  //   recorder.stopRecording(function () {
+  //     let blob = recorder.getBlob();
+  //     invokeSaveAsDialog(blob);
+  //   });
+  // });
+
+
+  setTimeout(async () => {
     await recorder.stop();
-    await browser.close();  
-  }, 50000)
+    await stream.destroy();
+    file.close();
+    console.log("finished");
+    await browser.close();
+  }, 15000)
 
 })();
